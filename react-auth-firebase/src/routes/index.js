6ca2a204fd6c14cb0e96654a0bcde333d5af907d
anangkf_ <gonanggoneng@gmail.com>
2022-10-27 10:49:27 +0700

    added register page

--- react-auth-firebase/src/routes/index.js
+++ react-auth-firebase/src/routes/index.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../components/Login";
+import Register from "../components/Register";
 
 const SetupRoutes = () =>{
 return(
     <BrowserRouter>
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
     </BrowserRouter>
 )
